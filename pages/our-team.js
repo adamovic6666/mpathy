c90b6e64bee32511537369ee07f9b12c7d099369
Slobodan Adamovic <slobodan@studiopresent.com>
2023-01-25 21:11:58 +0100

    hero changes

--- pages/our-team.js
+++ pages/our-team.js
@@ -9,7 +9,7 @@ const ourCrew = () => (
     <Hero
       title="<h1>Our team<h1/>"
       image="/assets/bhody-our-crew.png"
-      description="<p>We’ve assembled a diverse team of expert communicators, educationalists, psychologists, therapists, nutrionists, designers, application developers and social media marketing specialists.</p><p>In the backroom, there’s also a ‘focus –group’ of young people ensuring that we keep it real for their generation.</p><p>
+      description="<p>We’ve assembled a diverse team of expert communicators, educationalists, psychologists, therapists, nutrionists, designers, application developers and social media marketing specialists.</p><p>In the backroom, there’s also a ‘focus–group’ of young people ensuring that we keep it real for their generation.</p><p>
 Some of us are parents. All of us share a single passion and a total dedication – to help children, young people and their families join the Emotional Revolution and share happier, healthier futures.</p>"
     />
     <Crew />
