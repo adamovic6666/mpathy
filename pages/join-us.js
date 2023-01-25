9cf1d63b6f44f050b6cab85b57f8807e10706a3a
Slobodan Adamovic <slobodan@studiopresent.com>
2023-01-25 21:02:29 +0100

    hero changes

--- pages/join-us.js
+++ pages/join-us.js
@@ -5,9 +5,9 @@ import SEO from "../src/components/seo/SEO";
 const joinUs = () => {
   return (
     <>
-      <SEO metaTitle="Join us" metaDescription="" />
+      <SEO metaTitle="Get involved" metaDescription="" />
       <Hero
-        title="<h1>Join us<h1/>"
+        title="<h1>Get involved<h1/>"
         image="/assets/join-us-bhody.png"
         description="<p>Together, we can make the <span>Emotional Revolution</span> happen and literally change the shape of the future.</p><p>We need donors, partners and investors who are ready to join our mission
       downward spiral to medication and despair.</p>"
--- src/data/data.js
+++ src/data/data.js
@@ -2,5 +2,5 @@ export const NAV_LINKS = [
   { id: 1, name: "home", href: "" },
   { id: 2, name: "our mission", href: "our-mission" },
   { id: 3, name: "our team", href: "our-team" },
-  { id: 4, name: "join us", href: "join-us" },
+  { id: 4, name: "get involved", href: "get-involved" },
 ];
